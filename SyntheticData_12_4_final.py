721819c39c4db594ebce4fa7c6543018c104ed1a
frankiezeager <frankiezeager@yahoo.com>
2016-12-09 16:07:40 -0500

    Added coverage curve data code

--- SyntheticData_12_4_final.py
+++ SyntheticData_12_4_final.py
@@ -21,7 +21,7 @@ from itertools import cycle
 
 
 colnames = ['AUTH_ID','ACCT_ID_TOKEN','FRD_IND','ACCT_ACTVN_DT','ACCT_AVL_CASH_BEFORE_AMT','ACCT_AVL_MONEY_BEFORE_AMT','ACCT_CL_AMT','ACCT_CURR_BAL','ACCT_MULTICARD_IND','ACCT_OPEN_DT','ACCT_PROD_CD','ACCT_TYPE_CD','ADR_VFCN_FRMT_CD','ADR_VFCN_RESPNS_CD','APPRD_AUTHZN_CNT','APPRD_CASH_AUTHZN_CNT','ARQC_RSLT_CD','AUTHZN_ACCT_STAT_CD','AUTHZN_AMT','AUTHZN_CATG_CD','AUTHZN_CHAR_CD','AUTHZN_OPSET_ID','AUTHZN_ORIG_SRC_ID','AUTHZN_OUTSTD_AMT','AUTHZN_OUTSTD_CASH_AMT','AUTHZN_RQST_PROC_CD','AUTHZN_RQST_PROC_DT','AUTHZN_RQST_PROC_TM','AUTHZN_RQST_TYPE_CD','AUTHZN_TRMNL_PIN_CAPBLT_NUM','AVG_DLY_AUTHZN_AMT','CARD_VFCN_2_RESPNS_CD','CARD_VFCN_2_VLDTN_DUR','CARD_VFCN_MSMT_REAS_CD','CARD_VFCN_PRESNC_CD','CARD_VFCN_RESPNS_CD','CARD_VFCN2_VLDTN_CD','CDHLDR_PRES_CD','CRCY_CNVRSN_RT','ELCTR_CMRC_IND_CD','HOME_PHN_NUM_CHNG_DUR','HOTEL_STAY_CAR_RENTL_DUR','LAST_ADR_CHNG_DUR','LAST_PLSTC_RQST_REAS_CD','MRCH_CATG_CD','MRCH_CNTRY_CD','NEW_USER_ADDED_DUR','PHN_CHNG_SNC_APPN_IND','PIN_BLK_CD','PIN_VLDTN_IND','PLSTC_ACTVN_DT','PLSTC_ACTVN_REQD_IND','PLSTC_FRST_USE_TS','PLSTC_ISU_DUR','PLSTC_PREV_CURR_CD','PLSTC_RQST_TS','POS_COND_CD','POS_ENTRY_MTHD_CD','RCURG_AUTHZN_IND','RVRSL_IND','SENDR_RSIDNL_CNTRY_CD','SRC_CRCY_CD','SRC_CRCY_DCML_PSN_NUM','TRMNL_ATTNDNC_CD','TRMNL_CAPBLT_CD','TRMNL_CLASFN_CD','TRMNL_ID','TRMNL_PIN_CAPBLT_CD','DISTANCE_FROM_HOME']
-df = pd.read_csv('/Users/aksheetha/Documents/MSDS/capstone_research/training_part_10_of_10.txt', delimiter='|',header=None, names=colnames)
+df = pd.read_csv('/Applications/Graduate School/Fall 2016/Capstone/code/training_part_10_of_10.txt', delimiter='|',header=None, names=colnames)
 df1 = df.sample(n=1000000)
 df1.dtypes
 
@@ -118,6 +118,11 @@ for i in folds:
     
     mod_test = mod.predict(testcol)
     
+    #output coverage curve csv (predicted probabilities)
+    prob_test=mod.predict_proba(testcol)
+    prob_test=pd.DataFrame(prob_test)
+    prob_test.to_csv('/Applications/Graduate School/Fall 2016/Capstone/code/'+str(iteration_num)+'.csv')
+    
     cmfull=confusion_matrix(test['FRD_IND'],mod_test)    
     listFNR.append(cmfull[0][1])
     fpr, tpr, thresholds = metrics.roc_curve(test['FRD_IND'], mod_test, pos_label=2)
@@ -168,7 +173,9 @@ for i in folds:
     if (iteration_num+1)<len(folds):
         folds[iteration_num+1]=pd.concat([folds[iteration_num+1], fraud_trans], axis=0)
         iteration_num = iteration_num+ 1
-        print("iter")
+        print(iteration_num)
+    
+    
 
 #plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
 #plt.xlim([-0.05, 1.05])
