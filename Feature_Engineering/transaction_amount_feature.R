48a58f7f2012beacb4120bce563f779fd5cc2048
frankiezeager <frankiezeager@yahoo.com>
2017-01-31 18:06:35 -0500

    Fixed partial windows, uses num transactions instead

--- Feature_Engineering/transaction_amount_feature.R
+++ Feature_Engineering/transaction_amount_feature.R
@@ -1,26 +1,42 @@
 #difference from average transaction amount feature
-#uses a moving window of past 30/60/90 days to determine average transaction amount
+#uses a moving window of past n number of transactions to determine recent average transaction amount for an account
+
 library(readr)
 library(zoo)
 library(data.table)
+library(dplyr)
 library(plyr)
 library(RcppRoll)
 
-#read in the csv
+#read in the data
 col_names=c('AUTH_ID','ACCT_ID_TOKEN','FRD_IND','ACCT_ACTVN_DT','ACCT_AVL_CASH_BEFORE_AMT','ACCT_AVL_MONEY_BEFORE_AMT','ACCT_CL_AMT','ACCT_CURR_BAL','ACCT_MULTICARD_IND','ACCT_OPEN_DT','ACCT_PROD_CD','ACCT_TYPE_CD','ADR_VFCN_FRMT_CD','ADR_VFCN_RESPNS_CD','APPRD_AUTHZN_CNT','APPRD_CASH_AUTHZN_CNT','ARQC_RSLT_CD','AUTHZN_ACCT_STAT_CD','AUTHZN_AMT','AUTHZN_CATG_CD','AUTHZN_CHAR_CD','AUTHZN_OPSET_ID','AUTHZN_ORIG_SRC_ID','AUTHZN_OUTSTD_AMT','AUTHZN_OUTSTD_CASH_AMT','AUTHZN_RQST_PROC_CD','AUTHZN_RQST_PROC_DT','AUTHZN_RQST_PROC_TM','AUTHZN_RQST_TYPE_CD','AUTHZN_TRMNL_PIN_CAPBLT_NUM','AVG_DLY_AUTHZN_AMT','CARD_VFCN_2_RESPNS_CD','CARD_VFCN_2_VLDTN_DUR','CARD_VFCN_MSMT_REAS_CD','CARD_VFCN_PRESNC_CD','CARD_VFCN_RESPNS_CD','CARD_VFCN2_VLDTN_CD','CDHLDR_PRES_CD','CRCY_CNVRSN_RT','ELCTR_CMRC_IND_CD','HOME_PHN_NUM_CHNG_DUR','HOTEL_STAY_CAR_RENTL_DUR','LAST_ADR_CHNG_DUR','LAST_PLSTC_RQST_REAS_CD','MRCH_CATG_CD','MRCH_CNTRY_CD','NEW_USER_ADDED_DUR','PHN_CHNG_SNC_APPN_IND','PIN_BLK_CD','PIN_VLDTN_IND','PLSTC_ACTVN_DT','PLSTC_ACTVN_REQD_IND','PLSTC_FRST_USE_TS','PLSTC_ISU_DUR','PLSTC_PREV_CURR_CD','PLSTC_RQST_TS','POS_COND_CD','POS_ENTRY_MTHD_CD','RCURG_AUTHZN_IND','RVRSL_IND','SENDR_RSIDNL_CNTRY_CD','SRC_CRCY_CD','SRC_CRCY_DCML_PSN_NUM','TRMNL_ATTNDNC_CD','TRMNL_CAPBLT_CD','TRMNL_CLASFN_CD','TRMNL_ID','TRMNL_PIN_CAPBLT_CD','DISTANCE_FROM_HOME')
-df<-read.csv('/Users/frankiezeager/Documents/Graduate School/Capstone/training_part_10_of_10.txt',header=FALSE,sep='|',col.names=col_names)
-df1<-df[sample(nrow(df),10000),]
+
+df<-fread('/Users/frankiezeager/Documents/Graduate School/Capstone/training_part_10_of_10.txt',col.names=col_names)
+df<-as.data.frame(df)
+
 
 #change authzn date to date format
-df1$AUTHZN_RQST_PROC_DT=as.Date(df1$AUTHZN_RQST_PROC_DT)
+df$AUTHZN_RQST_PROC_DT=as.Date(df$AUTHZN_RQST_PROC_DT)
 
-#rolling mean of past 30 days of transactions (must have at least 5 transactions)
-df=as.data.table(df)
-setkey(df, "ACCT_ID_TOKEN", "AUTHZN_RQST_PROC_DT")
+# calculate rolling average for past 15 transactions by account number
+window=15
+#first order by group(account #) and date/time
+df = df[order(df$ACCT_ID_TOKEN,df$AUTHZN_RQST_PROC_DT,df$AUTHZN_RQST_PROC_TM), ]
+df2 = df %>%
+  #ignore the current value in the moving average
+  mutate(temp.lag1 = lag(AUTHZN_AMT, n = 1)) %>%
+  #use the previous 15 transactions, allowing for partial windows 
+  mutate(amt.mean.15 = rollapply(data = temp.lag1, 
+                                     width = window, 
+                                     FUN = mean, 
+                                     align = "right", 
+                                     partial=TRUE, 
+                                     na.rm = T))
 
-# calculate rolling average by account number
-df[, Roll.Avg.Trans.Amt := roll_meanl(AUTHZN_AMT, 30), by = ACCT_ID_TOKEN]
+#df2
 
-#calculate difference between average and current transaction amount
-df$Diff_Roll_Avg_Amt<-df$Roll.Avg.Trans.Amt-df$AUTHZN_AMT
+#test
+order_df<-df[order(ACCT_ID_TOKEN),]
+
+order_df$amt.mean.15
 
