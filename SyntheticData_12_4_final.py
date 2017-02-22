47a10701f4a599fca871a51ee1722b3ac747f0f7
frankiezeager <frankiezeager@yahoo.com>
2017-02-22 16:10:25 -0500

    check for positive examples, limit  to 3 strategies

--- SyntheticData_12_4_final.py
+++ SyntheticData_12_4_final.py
@@ -29,13 +29,15 @@ import matplotlib as mpl
 from sklearn.mixture import GaussianMixture
 from numpy import linspace,hstack
 from pylab import plot,show,hist
-
+from boto.s3.key import Key
 
 
 colnames = ['AUTH_ID','ACCT_ID_TOKEN','FRD_IND','ACCT_ACTVN_DT','ACCT_AVL_CASH_BEFORE_AMT','ACCT_AVL_MONEY_BEFORE_AMT','ACCT_CL_AMT','ACCT_CURR_BAL','ACCT_MULTICARD_IND','ACCT_OPEN_DT','ACCT_PROD_CD','ACCT_TYPE_CD','ADR_VFCN_FRMT_CD','ADR_VFCN_RESPNS_CD','APPRD_AUTHZN_CNT','APPRD_CASH_AUTHZN_CNT','ARQC_RSLT_CD','AUTHZN_ACCT_STAT_CD','AUTHZN_AMT','AUTHZN_CATG_CD','AUTHZN_CHAR_CD','AUTHZN_OPSET_ID','AUTHZN_ORIG_SRC_ID','AUTHZN_OUTSTD_AMT','AUTHZN_OUTSTD_CASH_AMT','AUTHZN_RQST_PROC_CD','AUTHZN_RQST_PROC_DT','AUTHZN_RQST_PROC_TM','AUTHZN_RQST_TYPE_CD','AUTHZN_TRMNL_PIN_CAPBLT_NUM','AVG_DLY_AUTHZN_AMT','CARD_VFCN_2_RESPNS_CD','CARD_VFCN_2_VLDTN_DUR','CARD_VFCN_MSMT_REAS_CD','CARD_VFCN_PRESNC_CD','CARD_VFCN_RESPNS_CD','CARD_VFCN2_VLDTN_CD','CDHLDR_PRES_CD','CRCY_CNVRSN_RT','ELCTR_CMRC_IND_CD','HOME_PHN_NUM_CHNG_DUR','HOTEL_STAY_CAR_RENTL_DUR','LAST_ADR_CHNG_DUR','LAST_PLSTC_RQST_REAS_CD','MRCH_CATG_CD','MRCH_CNTRY_CD','NEW_USER_ADDED_DUR','PHN_CHNG_SNC_APPN_IND','PIN_BLK_CD','PIN_VLDTN_IND','PLSTC_ACTVN_DT','PLSTC_ACTVN_REQD_IND','PLSTC_FRST_USE_TS','PLSTC_ISU_DUR','PLSTC_PREV_CURR_CD','PLSTC_RQST_TS','POS_COND_CD','POS_ENTRY_MTHD_CD','RCURG_AUTHZN_IND','RVRSL_IND','SENDR_RSIDNL_CNTRY_CD','SRC_CRCY_CD','SRC_CRCY_DCML_PSN_NUM','TRMNL_ATTNDNC_CD','TRMNL_CAPBLT_CD','TRMNL_CLASFN_CD','TRMNL_ID','TRMNL_PIN_CAPBLT_CD','DISTANCE_FROM_HOME']
-df = pd.read_csv('/Users/Aksheetha/Documents/MSDS/capstone_research/training_part_10_of_10.txt', delimiter='|',header=None, names=colnames)
-df1 = df.sample(n=1000000)
-df1.dtypes
+df = pd.read_csv('/Users/frankiezeager/Documents/Graduate School/Capstone/training_part_10_of_10.txt', delimiter='|',header=None, names=colnames)
+
+####### FOR AWS #########
+#read data from s3
+#combine together
 
 #first sort everything by date
 df1 = df1.sort_values(['AUTHZN_RQST_PROC_DT'])
@@ -139,7 +141,7 @@ for i in folds:
     prob_col_list=['index1','prob_0','prob_1','index2','truth_val','index3','pred_truth_val']
     prob_df.columns=prob_col_list
     prob_df=prob_df.drop(['index1','index2','index3'],axis=1)
-    path='/Users/Aksheetha/Documents/MSDS/capstone_research/code' #set path
+    path='/Users/frankiezeager/Documents/Graduate School/Capstone/' #set path
     prob_df.to_csv(path+'both_learn_coverage_'+str(iteration_num)+'.csv')
 ###############################################################################
 
@@ -163,23 +165,12 @@ for i in folds:
     #find best number of strategies:
     lowest_bic = np.infty
     bic = []
-    n_components_range = range(1, 4)
-    cv_types = ['spherical', 'tied', 'diag', 'full']
-    for cv_type in cv_types:
-        for n_components in n_components_range:
-            # Fit a Gaussian mixture with EM
-            gmm = mixture.GaussianMixture(n_components=n_components,
-                                          covariance_type=cv_type)
-            gmm.fit(strategy_df)
-            bic.append(gmm.bic(strategy_df))
-            if bic[-1] < lowest_bic:
-                lowest_bic = bic[-1]
-                print("current GMM is: ",gmm)
-                best_gmm = gmm
-    print("The optimal GMM is", best_gmm)
+          # Fit a Gaussian mixture with EM
+    gmm = mixture.GaussianMixture(n_components=3,covariance_type='full')
+    gmm.fit(strategy_df)
 
     #assign each transaction a strategy
-    strat_assign=best_gmm.predict(strategy_df)
+    strat_assign=gmm.predict(strategy_df)
 
     #attach back to data frame
     test['Strategy Number'] = strat_assign
@@ -188,7 +179,7 @@ for i in folds:
     for t, B_t in test.groupby('Strategy Number'):
         #B_t['batch_num'] = t
         #check for fraud transactions
-        if (sum(B_t['FRD_IND']) != 0):
+        if B_t['FRD_IND'].sum()> 0:
             all_batches.append(B_t)
 
     fn_rate = []
@@ -218,7 +209,7 @@ for i in folds:
     smoy = pd.DataFrame(smoy)
     syntheticdata = pd.concat((smox,smoy), axis=1)
     syntheticdata.columns=col_list
-
+    
     #take all of the synthetic data as an out-of-time sample
     allsynthetic_sets.append(syntheticdata)
 
