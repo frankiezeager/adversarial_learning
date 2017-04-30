c1373f1bf6d5217a35d2487b9afd839e26766d05
frankiezeager <frankiezeager@yahoo.com>
2017-04-30 14:53:22 -0400

    cleaned up code

--- adversarial_learning.py
+++ adversarial_learning.py
@@ -7,83 +7,66 @@ import numpy as np
 import math
 import random
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import cross_validation
-from sklearn.ensemble import VotingClassifier
 from sklearn.cross_validation import train_test_split
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import confusion_matrix
 from sklearn import metrics
 from sklearn.metrics import roc_curve, auc, roc_auc_score
 import matplotlib.pyplot as plt
-from itertools import cycle
 from sklearn import mixture
-from mpl_toolkits.mplot3d import Axes3D
-import itertools
-from scipy import linalg
-import matplotlib as mpl
-from sklearn.mixture import GaussianMixture
-from numpy import linspace,hstack
-from pylab import plot,show,hist,savefig
-import os
-import copy
-import pickle
 from sklearn.externals import joblib
+#this is only needed to output everything locally on AWS
 plt.switch_backend('agg')
 
+#set column names
 colnames = ['AUTH_ID','ACCT_ID_TOKEN','FRD_IND','ACCT_ACTVN_DT','ACCT_AVL_CASH_BEFORE_AMT','ACCT_AVL_MONEY_BEFORE_AMT','ACCT_CL_AMT','ACCT_CURR_BAL','ACCT_MULTICARD_IND','ACCT_OPEN_DT','ACCT_PROD_CD','ACCT_TYPE_CD','ADR_VFCN_FRMT_CD','ADR_VFCN_RESPNS_CD','APPRD_AUTHZN_CNT','APPRD_CASH_AUTHZN_CNT','ARQC_RSLT_CD','AUTHZN_ACCT_STAT_CD','AUTHZN_AMT','AUTHZN_CATG_CD','AUTHZN_CHAR_CD','AUTHZN_OPSET_ID','AUTHZN_ORIG_SRC_ID','AUTHZN_OUTSTD_AMT','AUTHZN_OUTSTD_CASH_AMT','AUTHZN_RQST_PROC_CD','AUTHZN_RQST_PROC_DT','AUTHZN_RQST_PROC_TM','AUTHZN_RQST_TYPE_CD','AUTHZN_TRMNL_PIN_CAPBLT_NUM','AVG_DLY_AUTHZN_AMT','CARD_VFCN_2_RESPNS_CD','CARD_VFCN_2_VLDTN_DUR','CARD_VFCN_MSMT_REAS_CD','CARD_VFCN_PRESNC_CD','CARD_VFCN_RESPNS_CD','CARD_VFCN2_VLDTN_CD','CDHLDR_PRES_CD','CRCY_CNVRSN_RT','ELCTR_CMRC_IND_CD','HOME_PHN_NUM_CHNG_DUR','HOTEL_STAY_CAR_RENTL_DUR','LAST_ADR_CHNG_DUR','LAST_PLSTC_RQST_REAS_CD','MRCH_CATG_CD','MRCH_CNTRY_CD','NEW_USER_ADDED_DUR','PHN_CHNG_SNC_APPN_IND','PIN_BLK_CD','PIN_VLDTN_IND','PLSTC_ACTVN_DT','PLSTC_ACTVN_REQD_IND','PLSTC_FRST_USE_TS','PLSTC_ISU_DUR','PLSTC_PREV_CURR_CD','PLSTC_RQST_TS','POS_COND_CD','POS_ENTRY_MTHD_CD','RCURG_AUTHZN_IND','RVRSL_IND','SENDR_RSIDNL_CNTRY_CD','SRC_CRCY_CD','SRC_CRCY_DCML_PSN_NUM','TRMNL_ATTNDNC_CD','TRMNL_CAPBLT_CD','TRMNL_CLASFN_CD','TRMNL_ID','TRMNL_PIN_CAPBLT_CD','DISTANCE_FROM_HOME']
+
 random.seed(1445)
 
+#initialize lists
 iteration_num = 0
-
 listFNR=[]
-#split each fold into training and testing sets
 best_strat_list=[]
 AUC_list=[]
 model_list=[]
-
 iter_num = 1
-
 allsynthetic_sets = []
 all_fold_oot=[]
 
 for i in range(1,11):
     #read in the data file
     df1=pd.read_csv('training_part_0'+str(i)+'_of_10.txt',delimiter='|',header=None, names=colnames)
-    #df1=pd.read_csv(str('/Users/frankiezeager/Documents/Graduate School/Capstone/'+'training_part_0'+str(i)+'_of_10.txt'),delimiter='|',header=None, names=colnames)
-    #df1=df1.sample(n=100000)
-    #df1=pd.read_csv('/Users/frankiezeager/Documents/Graduate School/Capstone/training_part_01_of_10.txt',delimiter='|',header=None, names=colnames)
-    #sort values by date
+    #make sure the df sorted by date
     df1 = df1.sort_values(['AUTHZN_RQST_PROC_DT'])
     
     #only select needed columns
-    #2-Fraud index (whether or not is fraud)
-    #7-ACCT_CUR_BAL
-    #14-Total Num. Authorizations
-    #18-authorization amount
-    #23-authorization outstanding amount
-    #30-Avg. Daily Authorization Amount
-    #44-Merchant Category ID (convert to factor)
-    #53-Plastic Issue Duration
-    #57- Point of Sale Entry Method (convert to factor)
-    #58 - Recurring Charge (Relabel as 0/1)
-    #68 - Distance from Home
+        #2-Fraud index (whether or not is fraud)
+        #7-ACCT_CUR_BAL
+        #14-Total Num. Authorizations
+        #18-authorization amount
+        #23-authorization outstanding amount
+        #30-Avg. Daily Authorization Amount
+        #44-Merchant Category ID (convert to factor)
+        #53-Plastic Issue Duration
+        #57- Point of Sale Entry Method (convert to factor)
+        #58 - Recurring Charge (Relabel as 0/1)
+        #68 - Distance from Home
     t_ind = [2, 7, 14, 18, 23, 30, 44, 53, 57, 58, 68]
     
-    #cols = pd.DataFrame(j.iloc[:,t_ind])
-    #subsetting relevant variables
+    #subset relevant variables
     df1 = pd.DataFrame(df1.iloc[:,t_ind])
     
     #take out any NaN's from the data set
     df1=df1.dropna(axis=0)
     
-    #convert these two columns from Y and N to 1 and 0,
+    #convert these two columns from Y and N to 1 and 0
     df1['FRD_IND'].replace(['Y', 'N'], [1, 0], inplace=True)
     df1['RCURG_AUTHZN_IND'].replace(['Y', 'N'], [1, 0], inplace=True)
     fraud_list=df1['FRD_IND']
     df1=df1.drop('FRD_IND',axis=1)
     df1['FRD_IND']=fraud_list
-
+    
+    #make a list of columns to use later
     col_list=df1.columns.values.tolist()
    #take out any NaN's from the data set
     df1=df1.dropna(axis=0)
@@ -95,33 +78,24 @@ for i in range(1,11):
     if i!=1:
         #find percent fraud in current df
         df_pct_fraud=sum(df1.FRD_IND==1)/len(df1)
-        #find difference between this and the 4% fraud desired
+        
+        #find difference between this and the 5% fraud desired
         pct_fraud_needed=.05-df_pct_fraud
-        #pct_fraud_needed=.15
+        
         #find number of fraud transactions needed
         num_fraud_trans=math.floor(pct_fraud_needed*len(df1))
-        #num_fraud_trans=num_fraud_trans.astype(int)
+        
         #finding the fraudulent transactions in synthetic data
         fraud_trans = syntheticdata[syntheticdata.iloc[:,-1] == 1]
+        
         #sampling the fraud transactions to include amount needed
         add_fraud = fraud_trans.sample(n=num_fraud_trans, replace=True,random_state=1575)
+        
         #adding fraud transactions back to df1
         df1=pd.concat([df1,add_fraud],axis=0)
 
     #split into training, 'testing' (finding the adversarial best strategy data frame), out of time (validation set)
     #60% training, 20% test, 20% validation
-    
-    #alternative method to find 60-20-20 train, test, validate but according to time order
-#    def train_test_validate_split(df, train_percent=.6, validate_percent=.2):
-#        m = len(df)
-#        train_end = int(train_percent * m)
-#        validate_end = int(validate_percent * m) + train_end
-#        train = df.iloc[:train_end]
-#        test = df.iloc[train_end:validate_end]
-#        out_of_time = df.iloc[validate_end:]
-#        return train, test, out_of_time
-#    
-#    train, test, out_of_time = train_test_validate_split(df1)
 
     train,intermediate_set=train_test_split(df1,train_size=.6,test_size=.4,random_state=1575)		
     test, out_of_time=train_test_split(intermediate_set, train_size=.5,test_size=.5,random_state=1575)	
@@ -130,7 +104,7 @@ for i in range(1,11):
     del intermediate_set
     
     
-     #filling any NAs 
+     #filling any NAs  (slicing the data frame sometimes inputs NAs)
     train=train.fillna(method='ffill')
     test=test.fillna(method='ffill')
     out_of_time=out_of_time.fillna(method='ffill')
@@ -144,22 +118,18 @@ for i in range(1,11):
     
     
     #convert column type to numeric
-    #df1['RCURG_AUTHZN_IND'] = df1['RCURG_AUTHZN_IND'].apply(pd.to_numeric)
-    #df1['FRD_IND'] = df1['FRD_IND'].apply(pd.to_numeric)
     out_of_time['RCURG_AUTHZN_IND'] = out_of_time['RCURG_AUTHZN_IND'].convert_objects(convert_numeric=True)
     out_of_time['FRD_IND'] = out_of_time['FRD_IND'].convert_objects(convert_numeric=True)
     all_fold_oot.append(out_of_time)
-    
-    test = test.sort_values(['AUTHZN_AMT']) #sorting by transaction amount
 
-#training the classifier
-#7-ACCT_CUR_BAL
-#14-Total Num. Authorizations
-#30-Avg. Daily Authorization Amount
-#44-Merchant Category ID (convert to factor)
-#57- Point of Sale Entry Method (convert to factor)
-#58 - Recurring Charge (Relabel as 0/1)
-#68 - Distance from Home
+#training the classifier (using the following vriables)
+    #7-ACCT_CUR_BAL
+    #14-Total Num. Authorizations
+    #30-Avg. Daily Authorization Amount
+    #44-Merchant Category ID (convert to factor)
+    #57- Point of Sale Entry Method (convert to factor)
+    #58 - Recurring Charge (Relabel as 0/1)
+    #68 - Distance from Home
     
     train_cols = train.drop('FRD_IND', axis=1) #columns for training
     
@@ -168,12 +138,13 @@ for i in range(1,11):
     logit = LogisticRegression(class_weight='balanced')
     
     mod = logit.fit(train_cols, train['FRD_IND'])
+    
+    #keep the models in a list to access later
     model_list.append(mod)
     testcol = test.drop('FRD_IND',axis=1)
-    #testcol=testcol.fillna(method='ffill')
     testcol.fillna(method='bfill',inplace=True)
     
-    #testcol[np.isnan(testcol)] = np.median(testcol[~np.isnan(testcol)])
+    #use the model to predict
     mod_test = mod.predict(testcol)
 
 ###############################################################################
@@ -200,7 +171,8 @@ for i in range(1,11):
     #find best number of strategies:
     lowest_bic = np.infty
     bic = []
-    # Fit a Gaussian mixture with EM
+    
+    # Fit a 6 component Gaussian mixture with EM
     gmm = mixture.GaussianMixture(n_components=6,covariance_type='full')
     gmm.fit(strategy_df)
 
@@ -211,24 +183,26 @@ for i in range(1,11):
     test['Strategy Number'] = strat_assign
               
     all_batches = []
+    
+    #cycle through the strategies created in separate dfs
     for t, B_t in test.groupby('Strategy Number'):
-        #B_t['batch_num'] = t
-        #check for fraud transactions
+        #check that there are fraudulent transactions in the batch
         if B_t['FRD_IND'].sum()> 0:
+            #append the batch to a list of batches (grouped by strategy number)
             all_batches.append(B_t)
 
     fn_rate = []
+    
+    #find batch with highest false negative rate (this will be our best strategy)
     for j in all_batches:
-        #cols=j.drop(j.columns[-1:-3], axis=1) 
         cols = j.drop(labels='Strategy Number',axis=1)
         cols = cols.drop(labels='FRD_IND',axis=1)
-        #cols=j.iloc[:,:-1] 
-        #cols=cols.iloc[:,:-1]
         cols.fillna(method='bfill',inplace=True)
         col_response = j.iloc[:,-2]
         pred = mod.predict(cols)
         cm = confusion_matrix(col_response, pred)
         FNR = cm[0][1]
+        #append false negative rate to a list of false negatives
         fn_rate.append(FNR)
     #find strategy with best false negative rate
     best_strat = fn_rate.index(max(fn_rate))
@@ -246,26 +220,19 @@ for i in range(1,11):
     smoy = pd.DataFrame(smoy)
     syntheticdata = pd.concat((smox,smoy), axis=1)
     syntheticdata.columns=col_list
-    #delete data frame to make more space in memory
-    #del df1
 
 
 
-#output files
+#output files to be used in curves.py
 val=1
 for file in all_fold_oot:
     file.to_csv('adv_learning_test_'+str(val)+'.csv')
     val=val+1
 
-#output models
-#for mod in range(3):
-#    joblib.dump(model_list[mod], 'adv_learning_model'+(str(mod))+'.pkl')
+#output models to be used in curves.py
 joblib.dump(model_list, 'adv_learning_models.pkl',compress=True)   
 
 ######################################## MODEL STAYS THE SAME, ADVERSARY CHANGES (no adv learning) ###############################################################################################################
-#make sure the seed is set
-random.seed(1445)      
-        
 iteration_num = 0
 
 listFNR=[]
@@ -283,7 +250,6 @@ all_fold_oot=[]
 for i in range(1,11):
     #read in the data file
     df1=pd.read_csv('/home/ec2-user/adversarial_learning/'+'training_part_0'+str(i)+'_of_10.txt',delimiter='|',header=None, names=colnames)
-    #df1=pd.read_csv(str('/Users/frankiezeager/Documents/Graduate School/Capstone/'+'training_part_0'+str(i)+'_of_10.txt'),delimiter='|',header=None, names=colnames)
     
     #take out any NaN's from the data set
     df1=df1.dropna(axis=0)
@@ -292,21 +258,21 @@ for i in range(1,11):
     df1 = df1.sort_values(['AUTHZN_RQST_PROC_DT'])
     
     #only select needed columns
-    #2-Fraud index (whether or not is fraud)
-    #7-ACCT_CUR_BAL
-    #14-Total Num. Authorizations
-    #18-authorization amount
-    #23-authorization outstanding amount
-    #30-Avg. Daily Authorization Amount
-    #44-Merchant Category ID (convert to factor)
-    #53-Plastic Issue Duration
-    #57- Point of Sale Entry Method (convert to factor)
-    #58 - Recurring Charge (Relabel as 0/1)
-    #68 - Distance from Home
+        #2-Fraud index (whether or not is fraud)
+        #7-ACCT_CUR_BAL
+        #14-Total Num. Authorizations
+        #18-authorization amount
+        #23-authorization outstanding amount
+        #30-Avg. Daily Authorization Amount
+        #44-Merchant Category ID (convert to factor)
+        #53-Plastic Issue Duration
+        #57- Point of Sale Entry Method (convert to factor)
+        #58 - Recurring Charge (Relabel as 0/1)
+        #68 - Distance from Home
     t_ind = [2, 7, 14, 18, 23, 30, 44, 53, 57, 58, 68]
     
-    #cols = pd.DataFrame(j.iloc[:,t_ind])
     df1 = pd.DataFrame(df1.iloc[:,t_ind])
+    
     #convert these two columns from Y and N to 1 and 0,
     df1['FRD_IND'].replace(['Y', 'N'], [1, 0], inplace=True)
     df1['RCURG_AUTHZN_IND'].replace(['Y', 'N'], [1, 0], inplace=True)
@@ -318,9 +284,7 @@ for i in range(1,11):
 
     col_list=df1.columns.values.tolist()
 
-#    #convert column type to numeric
-    #df1['RCURG_AUTHZN_IND'] = df1['RCURG_AUTHZN_IND'].apply(pd.to_numeric)
-    #df1['FRD_IND'] = df1['FRD_IND'].apply(pd.to_numeric)
+   #convert column type to numeric
     df1['RCURG_AUTHZN_IND'] = df1['RCURG_AUTHZN_IND'].convert_objects(convert_numeric=True)
     df1['FRD_IND'] = df1['FRD_IND'].convert_objects(convert_numeric=True)
    
@@ -328,10 +292,9 @@ for i in range(1,11):
     if i!=1:
         #find percent fraud in current df
         df_pct_fraud=sum(df1.FRD_IND==1)/len(df1)
-        #find difference between this and the 4% fraud desired
-        #pct_fraud_needed=.15
-        #find number of fraud transactions needed
+        #find difference between this and the 5% fraud desired
         pct_fraud_needed=.05-df_pct_fraud
+         #find number of fraud transactions needed
         num_fraud_trans=math.floor(pct_fraud_needed*len(df1))
         #num_fraud_trans=num_fraud_trans.astype(int)
         #finding the fraudulent transactions in synthetic data
@@ -344,24 +307,14 @@ for i in range(1,11):
     
     #split into training, 'testing' (finding the adversarial best strategy data frame), out of time (validation set)
     #60% training, 20% test, 20% validation
-
-    #alternative method to find 60-20-20 train, test, validate but according to time order
-#    def train_test_validate_split(df, train_percent=.6, validate_percent=.2, seed=1345):
-#        m = len(df)
-#        train_end = int(train_percent * m)
-#        validate_end = int(validate_percent * m) + train_end
-#        train = df.iloc[:train_end]
-#        test = df.iloc[train_end:validate_end]
-#        out_of_time = df.iloc[validate_end:]
-#        return train, test, out_of_time
-#    
-#    train, test, out_of_time = train_test_validate_split(df1)
     
     train,intermediate_set=train_test_split(df1,train_size=.6,test_size=.4,random_state=1575)		
     test, out_of_time=train_test_split(intermediate_set, train_size=.5,test_size=.5,random_state=1575)	
+    
     #delete intermediate set
     del intermediate_set
-
+    
+    #fill NAs
     train=train.fillna(method='ffill')
     test=test.fillna(method='ffill')
     out_of_time=out_of_time.fillna(method='ffill')
@@ -370,33 +323,18 @@ for i in range(1,11):
     #convert out of time data frame variables
     out_of_time.FRD_IND.map(dict(Y=1,N=0))
     out_of_time.RCURG_AUTHZN_IND.map(dict(Y=1,N=0))
-    #out_of_time['FRD_IND'].replace(['Y', 'N'], [1, 0], inplace=True)
-    #out_of_time['RCURG_AUTHZN_IND'].replace(['Y', 'N'], [1, 0], inplace=True)
-    
+
     #convert column type to numeric
     out_of_time['RCURG_AUTHZN_IND'] = out_of_time['RCURG_AUTHZN_IND'].convert_objects(convert_numeric=True)
     out_of_time['FRD_IND'] = out_of_time['FRD_IND'].convert_objects(convert_numeric=True)
     all_fold_oot.append(out_of_time)
     
     test = test.sort_values(['AUTHZN_AMT']) #sorting by transaction amount
-#training the classifier
-#7-ACCT_CUR_BAL
-#14-Total Num. Authorizations
-#30-Avg. Daily Authorization Amount
-#44-Merchant Category ID (convert to factor)
-#57- Point of Sale Entry Method (convert to factor)
-#58 - Recurring Charge (Relabel as 0/1)
-#68 - Distance from Home
-
     
-    train_cols = train.drop('FRD_IND', axis=1) #columns for training
-    
-    #Logistic Regression
-    #logit = LogisticRegression(class_weight='balanced')
+    #Logistic Regression (use the same model every time, so no need to train)
     
     #use the first model every time
     mod = model_list[0]
-    #model_list.append(mod)
     testcol = test.drop('FRD_IND',axis=1)
     testcol.fillna(method='bfill',inplace=True)
     mod_test = mod.predict(testcol)
@@ -438,19 +376,15 @@ for i in range(1,11):
               
     all_batches = []
     for t, B_t in test.groupby('Strategy Number'):
-        #B_t['batch_num'] = t
         #check for fraud transactions
         if B_t['FRD_IND'].sum()> 0:
             all_batches.append(B_t)
 
     fn_rate = []
-    for j in all_batches:
-        #cols=j.drop(j.columns[-1:-3], axis=1) 
+    for j in all_batches: 
         cols = j.drop(labels='Strategy Number',axis=1)
         cols = cols.drop(labels='FRD_IND',axis=1)
         cols.fillna(method='bfill',inplace=True)
-        #cols=j.iloc[:,:-1] 
-        #cols=cols.iloc[:,:-1]
         col_response = j.iloc[:,-2]
         pred = mod.predict(cols)
         cm = confusion_matrix(col_response, pred)
--- curves.py
+++ curves.py
@@ -9,33 +9,23 @@ Created on Thu Apr  6 18:29:49 2017
 
 
 import pandas as pd
-import numpy as np
-import math
-import random
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import cross_validation
-from sklearn.ensemble import VotingClassifier
-from sklearn.cross_validation import train_test_split
-from imblearn.over_sampling import SMOTE
-from sklearn.metrics import confusion_matrix
-from sklearn import metrics
 from sklearn.metrics import roc_curve, auc, roc_auc_score
 import matplotlib.pyplot as plt
 from itertools import cycle
-from sklearn import mixture
-from mpl_toolkits.mplot3d import Axes3D
-import itertools
-from scipy import linalg
-import matplotlib as mpl
-from sklearn.mixture import GaussianMixture
-from numpy import linspace,hstack
-from pylab import plot,show,hist,savefig
-import os
-import copy
-import pickle
 from sklearn.externals import joblib
+#output to local machine on AWS 
 plt.switch_backend('agg')
+
+
+# define coverage_curve function
+def coverage_curve(df, target_variable_col, predicted_prob_fraud_col, trxn_amount_col):
+    df=df.copy(deep=True)
+    df = df.sort_values(predicted_prob_fraud_col, ascending=False)
+    df['Fraud_Cumulative'] = df[target_variable_col].cumsum()*1.0 / df[target_variable_col].sum( )
+    df['TrxnCount'] = 1
+    df['Trxn_Cumulative'] = df['TrxnCount'].cumsum()*1.0 / df['TrxnCount'].sum( )
+  
+    return df
 
 ############### Adversarial Learning #####################################################################################################################################
 #read in the validation sets
@@ -147,20 +137,6 @@ for fold, model in  zip(adv_learning_oot, adv_learning_models):
 
 #### Coverage Curve for Adversarial Learning ####
   
-
-
-# define coverage_curve function
-def coverage_curve(df, target_variable_col, predicted_prob_fraud_col, trxn_amount_col):
-    df=df.copy(deep=True)
-    df = df.sort_values(predicted_prob_fraud_col, ascending=False)
-    df['Fraud_Cumulative'] = df[target_variable_col].cumsum()*1.0 / df[target_variable_col].sum( )
-    df['TrxnCount'] = 1
-    df['Trxn_Cumulative'] = df['TrxnCount'].cumsum()*1.0 / df['TrxnCount'].sum( )
-    #df['Exposure'] = df[trxn_amount_col] * df[target_variable_col]
-    #df['Exposure_Cumulative'] = df['Exposure'].cumsum()*1.0 / df['Exposure'].sum( )
-    #df['FPrate_Cumulative'] = (df['TrxnCount'].cumsum()*1.0 - df[target_variable_col].cumsum()) / df[target_variable_col].cumsum()
-  
-    return df
     
 ilist=[1,4,7,10]
 
@@ -182,8 +158,6 @@ for fold,model,color,i in zip(folds_list2,model_list2,colors,ilist):
     sorted_df.to_csv('adv_learning_coverage_'+str(val)+'.csv')
     #drop model_pred
     fold=fold.drop('model_pred',axis=1)
-    #reduce x axis
-    plt.xlim(0,.05)
     # produce chart
     plt.plot(sorted_df['Trxn_Cumulative'], sorted_df['Fraud_Cumulative'], color=color, label='Round '+str(i))
     plt.xlabel('Cumulative Transactions Examined')
@@ -247,18 +221,10 @@ for l, color in zip(folds_list, colors):
     l=l.copy(deep=True)
     syntheticdata_test=l.drop('FRD_IND',axis=1)
     syntheticdata_test=syntheticdata_test.drop(syntheticdata_test.columns[0],axis=1)
-    #syntheticdata_test=syntheticdata_test.drop('model_pred',axis=1)
-    #mod_test2 = z.predict(syntheticdata_test)
     mod_test3 = firstmod.predict_proba(syntheticdata_test)[:,1]
-    #cmfull=confusion_matrix(l['FRD_IND'],mod_test2)
     fpr, tpr, _ = roc_curve(l['FRD_IND'], mod_test3)
-    #fpr, tpr, thresholds = roc_curve(l['FRD_IND'], mod_test3, pos_label=2)
-    #print("The FNR is:", cmfull[0][1])
     print("The Plot AUC score is:", roc_auc_score(l['FRD_IND'],mod_test3 ))
-    #aucscore = roc_auc_score(l['FRD_IND'],mod_test2 )
-    #getting predicted probablilites for fraud
-    #mod_test3 = z.predict_proba(syntheticdata_test)[:,1]
-    #fpr1, tpr1, _ = roc_curve(l['FRD_IND'], mod_test3)
+
     aucscore = auc(fpr, tpr )
     plt.plot(fpr, tpr, lw=lw, color=color, label='ROC Round %d (area = %0.2f)' % (fold_n[i_num], aucscore))
     i_num += 1
@@ -273,7 +239,6 @@ plt.title('ROC Curve without Adversarial Learning')
 plt.legend(loc="lower right")
 #plt.show()
 
-#savefig('~/adversarial_learning/out_of_time_roc.png',bbox_inches='tight')
 plt.savefig('no_learning_roc.png',bbox_inches='tight')
 plt.savefig('no_learning_roc.svg',bbox_inches='tight')
 
@@ -286,7 +251,6 @@ for fold in no_learning_oot:
     model=firstmod
     syntheticdata_test=fold.drop('FRD_IND',axis=1)
     syntheticdata_test=syntheticdata_test.drop(syntheticdata_test.columns[0],axis=1)
-    #syntheticdata_test=syntheticdata_test.drop('model_pred',axis=1)
     mod_test3 = model.predict_proba(syntheticdata_test)[:,1]
     fpr, tpr, _ = roc_curve(fold['FRD_IND'], mod_test3)
     print("All Rounds No Learning AUC score is:", roc_auc_score(fold['FRD_IND'],mod_test3 ))  
@@ -311,8 +275,6 @@ for fold,color,i in zip(folds_list4,colors,ilist):
     sorted_df.to_csv('no_learning_coverage_'+str(val)+'.csv')
     #drop model_pred
     new_fold=new_fold.drop('model_pred',axis=1)
-    #reduce x axis
-    plt.xlim(0,.05)
     # produce chart
     plt.plot(sorted_df['Trxn_Cumulative'], sorted_df['Fraud_Cumulative'], color=color, label='Round '+str(i))
     plt.xlabel('Cumulative Transactions Examined')
